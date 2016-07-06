0d96eaf1724adafe1ce3beecb1308fe0ecfbb8b9
Jack Ye <yezhaoqin@berkeley.edu>
2016-07-05 23:57:20 -0700

    add holding analysis

--- Parser.py
+++ Parser.py
@@ -1,3 +1,4 @@
+import re, sqlite3
 
 '''
 Base class Parser used for detailed 
@@ -5,39 +6,35 @@ implementation of different textual parsers
 '''
 class Parser:
 
-	def __init__(self, text):
-		self.text = text
+	def __init__(self, path):
+		self.path = path
+		self.text = open(path).read()
 		self.facts = dict()
 
 	def analyze(self):
 		print(text)
 
 	def listFacts(self):
-		for (k, v) in facts:
+		for (k, v) in self.facts.items():
 			print('\033[1m', k, '\033[0m', v)
+
+	def register(self, db):
+		print("empty fact dictionary for database")
 
 
 class FactsheetParser(Parser):
 
 	def analyze(self):
-		self.facts["methodology"] = self.text.split("INDEX METHODOLOGY")[-1].split("ABOUT MSCI")[0]
-		self.facts["top10stocks"] = self.text.split("TOP 10 CONSTITUENTS")[-1].split("Total")[0].split("\n")
+		excludedRegex = "(^[0-9][.,0-9]+|^[0-9]$|TOP|MSCI|Weight|Mkt|Country|ESG|SRI|SECTOR|Momentum|Carbon|Fuels|^[A-Z][A-Z]$)"
 
-class ESGMethodologyParser(Parser):
-
-	def analyze(self):
-		# your code here
-
-
-class SRIMethodologyParser(Parser):
-
-	def analyze(self):
-		# your code here
-
-
-
-
-
+		self.facts["methodology"] = self.text.split("INDEX METHODOLOGY\n")[-1].split("ABOUT MSC\n")[0].replace("\n","")
+		stocks = list(filter(bool, self.text.split("Largest\nSmallest\nAverage\nMedian\n")[-1].split("Total\n")[0].split("\n")))
+		top10Stocks = list(filter(lambda s:not re.search(excludedRegex, s), stocks))
+		if not len(top10Stocks) <= 10:
+			raise Exception("not correct parsing", top10Stocks, len(top10Stocks))
+		else:
+			for i in range(1, len(top10Stocks)+1):
+				self.facts["top{}".format(i)] = top10Stocks[i-1]
 
 
 
--- parse.py
+++ parse.py
@@ -1,4 +1,9 @@
-import os
+import os, re, Parser
 
 for file in os.listdir("test2"):
-	FactsheetParser("test2/"+file).listFacts()
+	if re.search(".txt", file):
+		# print("Start parsing file", file)
+		parser = Parser.FactsheetParser("test2/"+file)
+		parser.analyze()
+		facts = parser.facts
+		print(file.replace(".txt","").replace("_"," ").upper(), facts.get("top1"), facts.get("top2"), facts.get("top3"), facts.get("top4"), facts.get("top5"), facts.get("top6"), facts.get("top7"), facts.get("top8"), facts.get("top9"), facts.get("top10"), sep=",")
